83b115c60f3ee6b7c12c1f628dd66916e81ae3f2
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-14 21:27:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-window.promises = [];
+// Array of promises with random delays
+const promises = [
+  new Promise((resolve) => setTimeout(() => resolve('Promise 1  resolved'), Math.random() * 4000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 2 resolved'), Math.random() * 4000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 3 resolved'), Math.random() * 4000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 4 resolved'), Math.random() * 4000 + 1000)),
+  new Promise((resolve) => setTimeout(() => resolve('Promise 5 resolved'), Math.random() * 4000 + 1000))
+];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+// Wait for the first promise to resolve using Promise.any()
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerText = result;
+  })
+  .catch((error) => {
+    console.log('Error:', error);
+  });
